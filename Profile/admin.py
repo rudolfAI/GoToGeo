88ffd916d726cab6513d816e9ac5bd1b2d61e37c
RUDOLF JACOBS <rudolf@a-i-solutions.co.za>
2022-03-03 13:21:03 +0200

    PR2.e - User admin
    [feat] logged in users can now only view
    thier own profile in the admin page

--- Profile/admin.py
+++ Profile/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Profile
+from django.contrib.auth.admin import UserAdmin
 
 
-admin.site.register(Profile)
+@admin.register(Profile)
+class CustomProfile(admin.ModelAdmin):
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        user = request.user
+        if request.user.is_superuser:
+            return Profile.objects.all()
+        else:
+            return Profile.objects.filter(id=request.user.profile.id)
